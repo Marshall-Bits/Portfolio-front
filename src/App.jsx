c5a3d08038f7e67b26d9a1bd903751071f4c64f6
marshall-bits <kz_@hotmail.es>
2024-03-25 19:30:49 +0100

    add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { useEffect } from 'react'
 import Hero from './components/Hero'
 import Menu from './components/Menu'
 import { Routes, Route } from 'react-router-dom'
@@ -9,14 +8,23 @@ import TalkingContainer from './components/TalkingContainer'
 import info from './info.json'
 import spaceShip from './assets/spaceship.webp'
 import cables from './assets/cables.webp'
+import moon from './assets/moon.webp'
+import redPlanet from './assets/red-planet.webp'
+import bluePlanet from './assets/blue-planet.webp'
+import purplePlanet from './assets/purple-planet.webp'
 
 function App() {
 
   return (
     <>
-      <div id='space'></div>
       <img id='spaceship' src={spaceShip} alt="spaceship" />
       <img id='cables' src={cables} alt="cables" />
+      <img id='moon' src={moon} alt="moon" />
+      <img id='red-planet' src={redPlanet} alt="red-planet" />
+      <img id='blue-planet' src={bluePlanet} alt="blue-planet" />
+      <img id='purple-planet' src={purplePlanet} alt="purple-planet" />
+      <div id='stars'  />
+      <div id='sky'></div>
       <Hero />
 
       <Routes>
@@ -25,7 +33,6 @@ function App() {
         <Route path="/career" element={<TalkingContainer text={info.career} />} />
         <Route path="/tools" element={<Tools />} />
       </Routes>
-      <div id='sky'></div>
       <Footer />
     </>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import './Footer.css';
+import bgLeft from '../assets/bg-mob-bottom-left.webp';
+import bgRight from '../assets/bg-mob-bottom-right.webp';
 
 function Footer() {
   return (
     <footer>
-     
+      <img src={bgLeft} alt="bg-left" className="bg-left" />
+      <img src={bgRight} alt="bg-right" className="bg-right" />
     </footer>
   );
 };
