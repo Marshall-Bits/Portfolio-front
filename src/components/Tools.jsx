328e68fbb9c489a62ecccdc38ae622709eb41085
marshall-bits <kz_@hotmail.es>
2024-03-27 10:24:08 +0100

    add changing title

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -26,11 +26,20 @@ function Tools() {
     const [playerStyle, setPlayerStyle] = useState({ bottom: 0, left: 0 });
     const [logoIndex, setLogoIndex] = useState(0);
     const [score, setScore] = useState(0);
+    const [displayedTechs, setDisplayedTechs] = useState([]);
+    const [title, setTitle] = useState('Catch all the techs I know!');
+
     const navigate = useNavigate();
+
+    // use ref to keep track of the current states in the checkCollision function (inside a setInterval)
+    const logoIndexRef = useRef(logoIndex);
+    const displayedTechsRef = useRef(displayedTechs);
     const playerRef = useRef(null);
     const rewardRef = useRef(null);
     const gameCanvasRef = useRef(null);
+
     const logos = [nodeLogo, expressLogo, vueLogo, piniaLogo, reactLogo, reduxLogo, csharpLogo, unityLogo];
+    const techs = ['Node.js', 'Express.js', 'Vue.js', 'Pinia', 'React.js', 'Redux', 'C#', 'Unity'];
 
     const loading = useImageLoader([...logos, playerImage, playerWin, canvasButton, buttonL, buttonR]);
 
@@ -57,43 +66,13 @@ function Tools() {
             ) {
                 reward.remove();
                 setScore((prevScore) => prevScore + 1);
+                if (!displayedTechsRef.current.includes(techs[logoIndexRef.current])) {
+                    setDisplayedTechs((prevTechs) => [...prevTechs, techs[logoIndexRef.current]]);
+                }
                 handelWin();
             }
         });
     }
-
-    useEffect(() => {
-        if (!loading) {
-            const interval = setInterval(() => {
-                setLogoIndex((currentIndex) => (currentIndex + 1) % logos.length);
-                const rewardPercentageWidth = (50 / gameCanvasRef.current.offsetWidth) * 100;
-                const newReward = {
-                    id: Math.random(),
-                    bottom: 100,
-                    left: Math.random() * (100 - rewardPercentageWidth)
-                };
-                setRewards((currentRewards) => [...currentRewards, newReward]);
-            }, 2000);
-
-            return () => clearInterval(interval);
-        }
-    }, [loading]);
-
-    useEffect(() => {
-        if (!loading) {
-            const interval = setInterval(() => {
-                setRewards((currentRewards) =>
-                    currentRewards.map((reward) => ({
-                        ...reward,
-                        bottom: reward.bottom - 5
-                    })).filter((reward) => reward.bottom > -100)
-                );
-                checkCollision();
-            }, 100);
-         
-            return () => clearInterval(interval);
-        }
-    }, [loading]);
 
     const renderRewards = () => {
         return rewards.map((reward) => (
@@ -134,14 +113,96 @@ function Tools() {
         }, 300);
     };
 
+    useEffect(() => {
+        if (!loading) {
+            const interval = setInterval(() => {
+                setLogoIndex((currentIndex) => (currentIndex + 1) % logos.length);
+                const rewardPercentageWidth = (50 / gameCanvasRef.current.offsetWidth) * 100;
+                const newReward = {
+                    id: Math.random(),
+                    bottom: 100,
+                    left: Math.random() * (100 - rewardPercentageWidth)
+                };
+                setRewards((currentRewards) => [...currentRewards, newReward]);
+            }, 2000);
+
+            return () => clearInterval(interval);
+        }
+    }, [loading]);
+
+    useEffect(() => {
+        if (!loading) {
+            const interval = setInterval(() => {
+                setRewards((currentRewards) =>
+                    currentRewards.map((reward) => ({
+                        ...reward,
+                        bottom: reward.bottom - 5
+                    })).filter((reward) => reward.bottom > -100)
+                );
+                checkCollision();
+            }, 100);
+
+            return () => clearInterval(interval);
+        }
+    }, [loading]);
+
+    useEffect(() => {
+        logoIndexRef.current = logoIndex;
+    }, [logoIndex]);
+
+    useEffect(() => {
+        displayedTechsRef.current = displayedTechs;
+    }, [displayedTechs]);
+
+    useEffect(() => {
+        switch (true) {
+            case score >= 100:
+                setScore(-9000);
+                break;
+            case score >= 70:
+                setTitle("Ok that's annoying. You know I can manipulate your score right?");
+                break;
+            case score >= 60:
+                setTitle("Literally, you can leave this open, touch nothing and your score will keep going up.");
+                break;
+            case score >= 50:
+                setTitle("This is not a real game. You don't event lose lives or points.");
+                break;
+            case score >= 40:
+                setTitle("Ok, you made your point. You're good at the easiest game in the world.");
+                break;
+            case score >= 30:
+                setTitle("Seriously, you can stop now. There's no end.");
+                break;
+            case score >= 20:
+                setTitle("I guess you've realized there's no end to this game...");
+                break;
+            case score >= 15:
+                setTitle('Wait, are you still playing?');
+                break;
+            case score >= 8:
+                setTitle('Well done! You caught all the techs!');
+                break;
+            case score < 0:
+                setTitle('Hahaha! I told you!');
+                break;
+            default:
+                break;
+        }
+
+    }, [score]);
+
     return (
         <div className={`container ${isFading && "slide-out"}`}>
 
             {loading ? <h2>Loading...</h2> :
                 <>
                     <section ref={gameCanvasRef} id='game-canvas'>
-                        <h3 className='score'>Catch all the technologies I know!</h3>
+                        <h3 className='score'>{title}</h3>
                         <h3 className='score'>Score: {score}</h3>
+                        <ul>
+                            {displayedTechs.map((tech, index) => <li key={index}>{tech}</li>)}
+                        </ul>
                         {renderRewards()}
                         <img src={playerSprite} ref={playerRef} style={playerStyle} id='player' />
                     </section>
